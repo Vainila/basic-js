05fe7769dd7f33d551719dcac9bf44c922a19fcc
Vainila <workmail1421@gmail.com>
2021-03-05 23:26:51 +0300

    add: implement carbon-dating, count-cats, dream-team, what-season tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,11 @@
 const CustomError = require("../extensions/custom-error");
-
+module.exports = function dateSample(string) {
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+let param = parseFloat(string);
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if(typeof string == 'string' &&  param>0 && param<=15){    
+      return  Math.ceil((Math.log(MODERN_ACTIVITY/(param)))/(0.693/HALF_LIFE_PERIOD));     
+  }
+  return false;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)){return false;}
+    let res = [];
+    for(let el of members){
+        if(typeof(el)=="string"){
+          el=el.trim();          
+          res.push(el[0].toUpperCase());
+        };
+    }
+    return res.sort().join("");
 };
