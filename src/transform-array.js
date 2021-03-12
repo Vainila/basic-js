a9eb277db2f9735229242a68d547f8f90256c68d
Vainila <workmail1421@gmail.com>
2021-03-12 21:05:01 +0300

    add: implement transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let cmd = ['--discard-next', `--discard-prev`,`--double-next`,`--double-prev`];  
+  if(!Array.isArray(arr)){throw Error;}
+  const arrCopy = [...arr];  
+  for(let i=0; i<arr.length; i++){
+    if(arrCopy[i]==cmd[0]){  
+      if(arrCopy[i+1]!=undefined && !cmd.includes(arrCopy[i+1])){
+        arrCopy[i+1]=undefined;
+      }
+    }
+    if(arrCopy[i]==cmd[1]){
+      if(arrCopy[i-1]!=undefined && !cmd.includes(arrCopy[i-1])){
+        arrCopy[i-1]=undefined;
+      }
+    }
+    if(arrCopy[i]==cmd[2]){
+      if(arrCopy[i+1]!=undefined && !cmd.includes(arrCopy[i+1])){
+        arrCopy[i]=arrCopy[i+1];
+      }
+    }
+    if(arrCopy[i]==cmd[3]){
+      if(arrCopy[i-1]!=undefined && !cmd.includes(arrCopy[i-1])){
+        arrCopy[i]=arrCopy[i-1];
+      }
+    }
+    if(cmd.includes(arrCopy[i]))arrCopy[i]=undefined;
+  }
+  let res = [];
+  arrCopy.map(elem => elem!=undefined ? res.push(elem) : {})
+  return res;
 };
